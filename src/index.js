bc219a886e9705a4b93649f85787ac50c821ff1d
TongoDaniel <leindaniel237@gmail.com>
2024-09-17 14:56:00 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 5000
 
 require('dotenv').config()
 
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
 app.use(bodyParser.json())
 
 app.use("/", serverCashierRoute)
